36b6f99422cbf45aa0950c0788cfcd7cf0c0fb23
ryosuke <20230175@tak.ed.jp>
2025-10-24 20:41:20 +0900

    アプリ起動部分のdebug=TrueをFalseに変更し、アプリの軽量化を行いました。

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def judge_promote(data):
         emit("illegal_move", {"msg": f"反則手です: {move}"}, room=room_number)
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+    socketio.run(app, host="127.0.0.1", port=5000, debug=False)
